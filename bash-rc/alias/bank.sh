c28560e179d260a20ceb57719eb3c62599ae5e42
Ravi J <raviregi@gmail.com>
2025-12-31 13:54:18 +0530

    updated browsers

---
+++ bash-rc/alias/bank.sh
@@ -0,0 +1,23 @@
+alias chrome='google-chrome --user-data-dir="$HOME/.config/google-chrome/ravi" &'
+
+alias hdfc='google-chrome --user-data-dir="$HOME/.config/google-chrome/ravi" https://www.hdfc.bank.in/ &'
+alias r-hdfc='google-chrome --user-data-dir="$HOME/.config/google-chrome/ravi" https://www.hdfc.bank.in/ &'
+alias a-hdfc='google-chrome --user-data-dir="$HOME/.config/google-chrome/arthi" https://www.hdfc.bank.in/ &'
+alias h-hdfc='google-chrome --user-data-dir="$HOME/.config/google-chrome/huf" https://www.hdfc.bank.in/ &'
+
+alias icici='google-chrome --user-data-dir="$HOME/.config/google-chrome/ravi" https://www.icici.bank.in/ &'
+alias r-icici='google-chrome --user-data-dir="$HOME/.config/google-chrome/ravi" https://www.icici.bank.in/ &'
+alias a-icici='google-chrome --user-data-dir="$HOME/.config/google-chrome/arthi" https://www.icici.bank.in &'
+alias h-icici='google-chrome --user-data-dir="$HOME/.config/google-chrome/huf" https://www.icici.bank.in &'
+
+alias axis='google-chrome --user-data-dir="$HOME/.config/google-chrome/ravi" https://www.axis.bank.in/ &'
+alias r-axis='google-chrome --user-data-dir="$HOME/.config/google-chrome/ravi" https://www.axis.bank.in/ &'
+alias a-axis='google-chrome --user-data-dir="$HOME/.config/google-chrome/arthi" https://www.axis.bank.in/ &'
+alias h-axis='google-chrome --user-data-dir="$HOME/.config/google-chrome/huf" https://www.axis.bank.in/ &'
+
+
+alias equitas='google-chrome --user-data-dir="$HOME/.config/google-chrome/arthi" https://equitas.bank.in/ &'
+alias r-equitas='google-chrome --user-data-dir="$HOME/.config/google-chrome/ravi" https://equitas.bank.in/ &'
+alias a-equitas='google-chrome --user-data-dir="$HOME/.config/google-chrome/arthi" https://equitas.bank.in/ &'
+alias h-equitas='google-chrome --user-data-dir="$HOME/.config/google-chrome/huf" https://equitas.bank.in/ &'
+
--- bash-rc/alias/taga.sh
+++ bash-rc/alias/taga.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # --- Infra ---
 alias taga-info='cat secret | grep taga'
-alias taga-domain='xdg-open https://www.godaddy.com &'
-alias taga-host='xdg-open https://hpanel.hostinger.com/vps/992688/overview &'
-alias taga-github='xdg-open https://www.github.com/nammataga &'
-alias taga-cloudflare='xdg-open https://dash.cloudflare.com &'
+alias taga-domain='firefox -P "ravi" https://www.godaddy.com &'
+alias taga-host='firefox -P "ravi" https://hpanel.hostinger.com/vps/992688/overview &'
+alias taga-github='firefox -P "ravi" https://www.github.com/nammataga &'
+alias taga-cloudflare='firefox -P "ravi" https://dash.cloudflare.com &'
 
 # --- Local Navigation ---
 alias tagaUi='cd ~/code/github/nammataga/prod/taga-ui'
