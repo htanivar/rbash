4996f644e7b2a54c68f1ec0f6875679020764ba7
Ravi J <raviregi@gmail.com>
2026-02-13 19:07:29 +0530

    added emulator

--- bash-rc/path/softwares.sh
+++ bash-rc/path/softwares.sh
@@ -127,10 +127,11 @@ for software in "${(@k)SOFTWARE}"; do
     check_and_add_path "$software"
 done
 
-
 export ANDROID_HOME=$HOME/Android/Sdk
-export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
+export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
+
 
 
 echo -e "${GREEN}PATH setup complete!${NC}"
