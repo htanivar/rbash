3069c85f73233e50733c89aed52245104e449fe2
Ravi J <raviregi@gmail.com>
2026-02-21 12:37:27 +0530

    added plantuml install

--- bash-rc/config/set-ssh.sh
+++ bash-rc/config/set-ssh.sh
@@ -2,10 +2,12 @@
 
 
 RAVI_LINUX="/home/ravi/.ssh/id_rsa"
+AI_LINUX="/home/ravi/.ssh/ai-linux_id"
 
 # --- SSH Agent ---
 # Start the SSH agent and add the SSH key if not already running
 if [ -z "$SSH_AUTH_SOCK" ]; then
     eval "$(ssh-agent -s)"
     ssh-add $RAVI_LINUX
+    ssh-add $AI_LINUX
 fi
