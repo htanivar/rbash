a23f20413487f0326f772d2666a6af65d2036e66
Ravi J <raviregi@gmail.com>
2026-03-12 18:24:50 +0530

    renamed ghop

--- bash-rc/link/soft.sh
+++ bash-rc/link/soft.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ln -sf /home/ravi/code/github/htanivar/technical-notes/002_rsys/batch-scripts/sh/docker-op.sh ~/bin/dop
-ln -sf /home/ravi/code/github/htanivar/technical-notes/002_rsys/batch-scripts/sh/gh-ops.sh ~/bin/gh
+ln -sf /home/ravi/code/github/htanivar/technical-notes/002_rsys/batch-scripts/sh/gh-ops.sh ~/bin/ghop
 ln -sf /home/ravi/code/github/htanivar/rbash/scripts/git/compare-branches.sh ~/bin/git-compare-branches
