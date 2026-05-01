13c02ea7354c5889b9ba6cd15a3b5d3c22c89f42
Ravi J <raviregi@gmail.com>
2026-05-02 01:44:40 +0530

    commited path

--- bash-rc/link/soft.sh
+++ bash-rc/link/soft.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -sf /home/ravi/code/github/htanivar/technical-notes/002_rsys/batch-scripts/sh/docker-op.sh ~/bin/dop
-ln -sf /home/ravi/code/github/htanivar/technical-notes/002_rsys/batch-scripts/sh/gh-ops.sh ~/bin/ghop
-ln -sf /home/ravi/code/github/htanivar/technical-notes/002_rsys/batch-scripts/sh/gl-ops.sh ~/bin/glop
-ln -sf /home/ravi/code/github/htanivar/rbash/scripts/git/compare-branches.sh ~/bin/git-compare-branches
+ln -sf $HOME/code/github/htanivar/technical-notes/002_rsys/batch-scripts/sh/docker-op.sh ~/bin/dop
+ln -sf $HOME/code/github/htanivar/technical-notes/002_rsys/batch-scripts/sh/gh-ops.sh ~/bin/ghop
+ln -sf $HOME/code/github/htanivar/technical-notes/002_rsys/batch-scripts/sh/gl-ops.sh ~/bin/glop
+ln -sf $HOME/code/github/htanivar/rbash/scripts/git/compare-branches.sh ~/bin/git-compare-branches
